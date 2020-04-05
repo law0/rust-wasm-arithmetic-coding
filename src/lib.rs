26ad1dac3780d0ac2d86ea702359cd88c465eda8
law <lawrencio@outlook.fr>
2020-04-05 16:01:34 +0200

    Add compression of arbitrary length strings.
    Compression is made by blocks. The compression loop repeats until the difference between
    the b_inf and b_sup is comparable to std::f64::EPSILON.
    As such I obtained to compress on average blocks of 8 to 13 bytes into a 8 bytes floating number.
    Compression ratio obtained is generally around 0.70.
    
    Problem to solve, the uncompress func still needs to know how much bytes it needs to
    uncompress for each block. There must be a way to pass this info within the compressed data.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+use std::mem;
+
 extern crate web_sys;
 
 #[macro_export]
@@ -37,8 +39,8 @@ pub struct WasmCompress {
     orig_string: String,
     result_string: String,
     f: f64,
-    byte_len: u8,
     interv: BTreeMap<u8, (f64, f64) >, //because we need a total order
+    compression_ratio: f64,
 }
 
 #[wasm_bindgen]
@@ -50,8 +52,8 @@ impl WasmCompress {
             orig_string: String::new(),
             result_string: String::new(),
             f: 0.0,
-            byte_len: 0,
             interv: BTreeMap::new(),
+            compression_ratio: 0.0,
         }
     }
 
@@ -70,22 +72,52 @@ impl WasmCompress {
     pub fn set_string(&mut self, s: &str) {
         self.orig_string.clear();
         self.orig_string.push_str(&s);
-        self.analyze();
-        self.compress();
-        self.uncompress();
+        self.proceed();
     }
 
     pub fn get_result_float(&self) -> f64 {
         log!("returning {}", self.f);
         self.f
     }
+
+    pub fn get_compression_ratio(&self) -> f64 {
+        self.compression_ratio
+    }
 }
 
 impl WasmCompress {
 
+    fn proceed(&mut self) {
+        self.analyze();
+
+        let bytes : &[u8] = self.orig_string.as_bytes();
+        let mut result_str : Vec<u8> = Vec::new();
+
+        let mut i = 0;
+        let mut count = 0;
+        while i < bytes.len() {
+            //let mut end = i + 10;
+            //if end > bytes.len() { end = bytes.len(); }
+            let (c, f) = self.compress(&bytes[i..]).unwrap();
+            self.f = f;
+            log!("Compressed {} bytes", c);
+            i += c;
+            result_str.append(&mut self.uncompress(c, self.f));
+
+            count += 1;
+        }
+
+        self.result_string = match String::from_utf8(result_str) {
+            Ok(s) => s,
+            Err(e) => e.to_string(),
+        };
+
+        self.compression_ratio = (count * mem::size_of::<f64>()) as f64 / bytes.len() as f64;
+    }
+
     fn analyze(&mut self) {
         let bytes : &[u8] = self.orig_string.as_bytes(); 
-        self.byte_len = bytes.len() as u8; //won't get string bigger than 255 bytes anyway, due to float precision, it won't get much than 15 utf-8 chars
+        let byte_len = bytes.len() as u128; //won't get string bigger than 255 bytes anyway, due to float precision, it won't get much than 15 utf-8 chars
         self.interv.clear();
 
         for c in bytes {
@@ -98,63 +130,65 @@ impl WasmCompress {
 
         let mut acc = 0.0;
         let mut prev_acc = 0.0;
-        let lenf = self.byte_len as f64;
+        let lenf = byte_len as f64;
 
         for value in self.interv.values_mut() { //return &mut (f64, f64)
-            acc += value.1 / lenf;
+            acc += value.1 / lenf * (1.0 - std::f64::EPSILON);
             *value = (prev_acc, acc);
             log!("({} {})", value.0, value.1);
             prev_acc = acc;
         }
     }
 
-    fn compress(&mut self) {
-        let bytes : &[u8] = self.orig_string.as_bytes(); 
+    fn compress(&self, data: &[u8]) -> Result<(usize, f64), &'static str> {
         let mut b_inf : f64 = 0.0;
         let mut b_sup : f64 = 1.0;
         let mut delta : f64;
+        let mut counter : usize = 0;
+        let mut f : f64 = 0.0;
 
-        for symbol in bytes {
+        for symbol in data {
             if let Some(bornes) = self.interv.get(symbol) {
                 delta = b_sup - b_inf;
                 b_sup = b_inf + delta * bornes.1;
                 b_inf = b_inf + delta * bornes.0;
                 log!("delta {} = {} - {} -> {}", delta, b_sup, b_inf, symbol);
+                let diff = b_sup - b_inf;
+                if diff <= std::f64::EPSILON {
+                    break;
+                } else {
+                    f = b_inf + std::f64::EPSILON;
+                    counter += 1;
+                }
             } else {
-                log!("Couldn't find first char in interv hash map, something must have been gone wrong");
-                panic!("Exited due to previous error")
+                //Explicit return cause otherwise it tries to return just out of the if
+                return Err("Couldn't find first char in interv hash map, something must have been gone wrong")
             }
         }
 
-        self.f = b_inf + (b_sup - b_inf) / 2.0; //TODO find smallest floating number representation
+        Ok((counter, f))
     }
 
-    fn uncompress(&mut self) {
-        self.result_string.clear();
+    fn uncompress(&self, size: usize, mut f: f64) -> Vec<u8> {
         let mut result_str: Vec<u8> = Vec::new();
 
-        let mut fval = self.f;
-        for _ in 0..self.byte_len {
+        for _ in 0..size {
             for key_val in &self.interv {
 
                 let b_inf = (key_val.1).0;
                 let b_sup = (key_val.1).1;
 
-                if b_inf < fval && fval < b_sup {
-                    log!("{} < fval = {} < {} -> {}", b_inf, fval, b_sup, *key_val.0);
+                if b_inf < f && f < b_sup {
+                    log!("{} < f = {} < {} -> {}", b_inf, f, b_sup, *key_val.0);
                     result_str.push(*key_val.0);
                     
-                    fval = (fval - b_inf) / (b_sup - b_inf);
-                    log!("new fval = {}", fval);
+                    f = (f - b_inf) / (b_sup - b_inf);
+                    log!("new f = {}", f);
                     break;
                 }
             }
         }
 
-        unsafe {
-            self.result_string = String::from_utf8_unchecked(result_str);
-        }
+        result_str
     }
 }
-
-
--- www/index.js
+++ www/index.js
@@ -5,9 +5,11 @@ const wasmCompress = new WasmCompress()
 const myFunction = () => {
   var inputText = document.getElementById('theString').value
   wasmCompress.set_string(inputText)
-  document.getElementById('demo').innerHTML = 'Original string : ' + wasmCompress.get_string() + '<br>'
-  document.getElementById('demo').innerHTML += 'uncompressed string :' + wasmCompress.get_result_string() + '<br>'
-  document.getElementById('demo').innerHTML += 'compressed float :' + wasmCompress.get_result_float()
+  document.getElementById('orig').innerHTML = 'Original string : ' + wasmCompress.get_string()
+  document.getElementById('result').innerHTML = 'result string : ' + wasmCompress.get_result_string()
+  document.getElementById('f').innerHTML = 'last compressed float :' + wasmCompress.get_result_float()
+
+  document.getElementById('ratio').innerHTML = 'compression ratio : ' + wasmCompress.get_compression_ratio()
 
   document.getElementById('tree').innerHTML = wasmCompress.get_tree_string()
 }
